a7790fc36bb963e1292e662277ebe76301ec70a5
Steven Allen <steven@stebalien.com>
2020-04-15 17:49:34 -0700

    chore: add a go module file

--- util/stack.go
+++ util/stack.go
@@ -21,8 +21,9 @@ type Stack struct {
 
 func (s *Stack) Print() {
 	state := s.State
-	if s.WaitTime != 0 {
-		state += ", " + s.WaitTime.String()
+	waitTime := int(s.WaitTime.Minutes())
+	if waitTime != 0 {
+		state += ", " + fmt.Sprintf("%d minutes", waitTime)
 	}
 	fmt.Printf("goroutine %d [%s]:\n", s.Number, state)
 	for _, f := range s.Frames {
